d17a77b67d5b830685fcb1d917b009949dda7606
CassandraBd <bodencassandra@gmail.com>
2021-01-18 16:36:59 -0600

    First commit on practice1

--- src/Day1/Practice1.java
+++ src/Day1/Practice1.java
@@ -1,7 +1,8 @@
+package Day1;
+
 public class Practice1 {
     public static void main(String[] args) {
 
-        System.out.println("Hello");
-        // Seeing it this works
+        System.out.println("First Try, Day 1");
     }
 }
